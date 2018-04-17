224d4f6222aeb02ebb54c2845a789ddca0cbfa38
Ryan Grenz <info@ryangrenz.com>
2018-04-17 13:36:33 +0100

    Add short hostname to /etc/hosts to allow service to start
    This is required when container hostname is longer than 15 chars,
    otherwise the service will only start on IPC, not port 1433.
    
    Refer to bottom of this ticket for more info:
    https://github.com/smootoo/freeslick/issues/1

--- sql2000/start.sh
+++ sql2000/start.sh
@@ -1,2 +1,8 @@
 cd /root
+
+ip=$(/sbin/ifconfig eth0 | grep 'inet addr' | cut -d: -f2 | awk '{print $1}')
+hostname=$(hostname)
+short_hostname=${hostname:0:15}
+echo "${ip} ${short_hostname}" >> /etc/hosts
+
 /usr/bin/wine .wine/drive_c/Program\ Files/Microsoft\ SQL\ Server/80/Tools/Binn/sqlmangr.exe
